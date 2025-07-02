9bb8764af8640edd23a479e2858d713054f0c97a
Inferm-0 <aidanmaloney6464@gmail.com>
2025-07-02 15:00:45 -0500

    Add files via upload

---
+++ rogue-like-game/scripts/lawnmower_basic.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var SPEED = 100
+var player_chase = false 
+var player = null
+
+
+
+func _on_detectionarea_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true 
+
+func _on_detectionarea_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
+	
+func _physics_process(delta: float) -> void:
+	if player_chase:
+		position += ((player.position - position)/(player.position.distance_to(position)))*delta*SPEED
+		
+		#$AnimatedSprite2D.play
+		
+		if(player.position.x - position.x) <0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		pass
+		#$AnimatedSprite2D.play
+
+		
+	
