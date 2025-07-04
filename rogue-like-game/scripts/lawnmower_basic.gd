649dd110471e47cb36e37a38e14dc3ced9ed2992
Inferm-0 <aidanmaloney6464@gmail.com>
2025-07-04 12:59:18 -0500

    Add files via upload

--- rogue-like-game/scripts/lawnmower_basic.gd
+++ rogue-like-game/scripts/lawnmower_basic.gd
@@ -18,15 +18,15 @@ func _physics_process(delta: float) -> void:
 	if player_chase:
 		position += ((player.position - position)/(player.position.distance_to(position)))*delta*SPEED
 		
-		#$AnimatedSprite2D.play
+		
 		
 		if(player.position.x - position.x) <0:
-			$AnimatedSprite2D.flip_h = true
-		else:
 			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
 	else:
 		pass
-		#$AnimatedSprite2D.play
+	
 
 		
 	
--- rogue-like-game/scripts/player.gd
+++ rogue-like-game/scripts/player.gd
@@ -2,13 +2,33 @@ extends CharacterBody2D
 
 
 const SPEED = 175.0
-
+var down = Vector2(0.0 ,1.0)
+var up = Vector2(0.0 ,-1.0)
 
 	
 func  get_input():
 	var input_direction = Input.get_vector("move left","move right","move up","move down")
 	velocity = input_direction * SPEED
+
+func animation_direction():
+	var direction = "down"
+	if velocity.y < 0: direction = "up"
+	
+	elif velocity.y > 0: direction = "down"
+	
+	if velocity.y>0 or velocity.y<0 or velocity.x >0 or velocity.x <0:
+		$AnimatedSprite2D.play("move_" + direction)
+	elif velocity.y == 0:
+		$AnimatedSprite2D.play("idle_" + direction)
+	
+	
+	
+	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	animation_direction()
+	
+	
+	
 	
